596768d472f85a905d6cc9de2e8bf8c959671a75
mohammadwh <M@hdimanager88>
2024-01-26 13:57:33 +0330

    add file

---
+++ Dcommerce/Customers/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+# Create your models here.
+
+class Customers(AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_active = False
+    telephone = models.CharField(max_length=100)
+
+class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='address')
+    address_line1 = models.CharField(max_length=100)
+    address_line2 = models.CharField(max_length=100,blank=True)
+    city = models.CharField(max_length=100,blank=True)
+    postal_code = models.CharField(max_length=100,blank=True)
+    country = models.CharField(max_length=100,blank=True)
+    telephone = models.CharField(max_length=100,blank=True)
+    mobile = models.CharField(max_length=100,blank=True)
+
+class
--- Dcommerce/settings.py
+++ Dcommerce/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #defult Dcommerce apps
+
+
+    "Dcommerce.Customers",
+
+
 ]
 
 MIDDLEWARE = [
